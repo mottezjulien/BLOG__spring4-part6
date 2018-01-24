6ad3005a18b08c2332966dc2c1559e42a5d42a8d
Julien <julien@linxo.com>
2018-01-24 11:18:48 +0100

    [Update] Remove Spring Data

--- src/main/java/fr/jmottez/lessons/spring/spring4/generic/repository/CrudRepository.java
+++ src/main/java/fr/jmottez/lessons/spring/spring4/generic/repository/CrudRepository.java
@@ -1,13 +1,11 @@
 package fr.jmottez.lessons.spring.spring4.generic.repository;
 
 
-import org.springframework.data.repository.Repository;
-
 import java.io.Serializable;
 import java.util.Collection;
 
 
-public interface CrudRepository<Entity, ID extends Serializable> extends Repository<Entity, ID> {
+public interface CrudRepository<Entity, ID extends Serializable> {
 
 	<EntityChild extends Entity> EntityChild save(EntityChild entity) throws RepositoryException;
 
